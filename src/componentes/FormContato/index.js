cf1d731de35265c3ad88221a911cbc5888a0354e
matheusjesse <matheus_jesse@hotmail.com>
2022-05-08 20:31:48 -0300

    implementando css na pagina de enviar menssagem implementando formulario para enviar e-mail e button disbled função

--- src/componentes/FormContato/index.js
+++ src/componentes/FormContato/index.js
@@ -1,39 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FormContatoContainer from './style';
 
 function FormContato() {
+  const [ inputValues, setInputValues] = useState({
+      name: '',
+      email: '',
+      message: '',
+    }); 
+  const [ loading, setLoading ] = useState(false);
+  const [ isDisabled, setDisabled ] = useState(true);
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+ 
+  const handleSubmit = async () => {    
+    setLoading(true);
+    await delay(5000);
+    setLoading(false);   
+  }
+
+  useEffect(() => {
+    validationButton();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inputValues]);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;   
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    })   
+  }
+
+  const validationButton = () => {
+    const lengthValidation = inputValues.name.length > 4 
+    && inputValues.message.length >= 8;
+    const regex = /[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]*\w$/;
+    const validationEmail = regex.test(email);
+    const validation = lengthValidation && validationEmail;
+    if (validation) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }
+
+  const { name, email, message } = inputValues;
   return(
     <FormContatoContainer>
       <h1>Menssagem</h1>
-      <form action="POST" id="form-contato">
-        <label htmlFor="name-input">
-          <input 
+      <form
+        action="https://formsubmit.co/2a71ee4e83d2a63d27b27cd872e84c5b"
+        id="form-contato"
+        method="POST"
+      >
+        <label htmlFor="name">
+          <input
             type="text"
-            id="name-input"
-            name="name-input"
+            id="name"
+            name="name"
             placeholder="Nome"
+            value={ name }
+            onChange={ handleChange }
+            readOnly={ loading ? "readonly" : '' }
+            autocomplete="off"
           />
         </label>
-        <label htmlFor="email-input">
-          <input 
+        <label htmlFor="email">
+          <input
             type="text"
-            id="email-input"
-            name="email-input"
+            id="email"
+            name="email"
             placeholder="E-Mail"
+            value={ email }
+            onChange={ handleChange }
+            readOnly={ loading ? "readonly" : '' }
+            autocomplete="off"
           />
-        </label>        
-      </form>
-      <div className="menssagem-e-button">
-        <label htmlFor="text-area-messagem">
-          <textarea 
-            name="text-area-messagem"
-            id="text-area-messagem"
-            form="form-contato"
-            placeholder="Menssagem..."
-          />                  
         </label>
-        <button type="button">ENVIAR</button>
-      </div>
+        <div className="menssagem-e-button">
+          <label htmlFor="text-area-messagem">
+            <textarea
+              name="message"
+              id="message"
+              form="form-contato"
+              placeholder="Menssagem..."
+              value={ message }
+              onChange={ handleChange }
+              readOnly={ loading ? "readonly" : '' }
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={ handleSubmit }
+            disabled={ isDisabled }
+          >
+            ENVIAR
+          </button>
+        </div>
+        <input type="hidden" name="_captcha" value="false" />
+        <input type="hidden" name="_next" value="http://localhost:3000/contato" />
+      </form>
+      { loading ? (<h1>ENVIANDO...</h1>) : '' }
+      
     </FormContatoContainer>    
   );
 }
--- src/componentes/FormContato/style.js
+++ src/componentes/FormContato/style.js
@@ -1,54 +1,99 @@
 import styled from 'styled-components';
 
 const FormContatoContainer = styled.div`
-  height: 400px;
-  width: 500px;
+  height: 420px;
+  width: 420px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(7.6px);
+  -webkit-backdrop-filter: blur(7.6px);   
+  border-radius: 10px;
+  background: #F5F5F5;
 
   form {
     display: flex;
     flex-direction: column;
     align-items: center;
+    
   }
 
+  label {
+    background-color: #F5F5F5;
+  }
   .menssagem-e-button {    
     display: flex;
     flex-direction: column;
+    background-color: #F5F5F5;
   }
 
+  h1 {
+    background: #F5F5F5;
+    color: black;
+  }
   input {
     width: 300px;
     height: 48px;
     border: none;
-    background-color: white;
+    background: #F5F5F5;
+   
     border-radius: 8px;
     margin: 4px 0px 8px;
+    color: #001319;
+    font-size: 22px;
+    padding-left: 6px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(7.6px);
+    -webkit-backdrop-filter: blur(7.6px);   
   }
 
   textarea {
     width: 300px;
     height: 140px;
     border: none;
-    background-color: white;
+    background: #F5F5F5;
     border-radius: 8px;
     margin: 4px 0px 8px;
+    font-size: 22px;
+    padding-left: 6px;
+    color: #001319;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(7.6px);
+    -webkit-backdrop-filter: blur(7.6px);   
   }
+
+  input::placeholder {
+    color: #405157;
+  }
+
+  textarea::placeholder {
+    color: #405157;
+  }
+
+  textarea:focus, input:focus{
+    outline: none;
+    border: 1px solid #00638A;
+  }
+
 
   button {
     height: 40px;
     border-radius: 10px;
     border: 1px solid white;
-    background-color: #001319;
-    font-size: 24px;
+    background-color: #00476D;
+    font-size: 24px;    
   }
 
   button:hover {
-    background-color:#00476D;
+    background-color:#00638A;
     border: none;    
   }
   
+  button:disabled {
+        background-color: #405157;
+        color: white;
+    } 
 `;
 
 export default FormContatoContainer;

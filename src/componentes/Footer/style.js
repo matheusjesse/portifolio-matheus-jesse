c0119175d3b10fa453bc07f84bd8e4f941779dc4
matheus jesse <matheus_jesse@hotmail.com>
2022-05-20 23:28:48 -0300

    alteração nos icons do footer e na border shadow

--- src/componentes/Footer/style.js
+++ src/componentes/Footer/style.js
@@ -1,20 +1,18 @@
 import styled from 'styled-components';
-
-const FooterContainer = styled.footer` 
-  background-color:  #F5F5F5;
+// background-color:  #F5F5F5;
+const FooterContainer = styled.footer`  
   height: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: 0px 2px 4px -1px white;
 
   img {
     width: 60px;
-    height: 60px;
-    background-color:  #F5F5F5;
+    height: 60px;    
   }
 
-  a {
-    background-color:  #F5F5F5;
+  a {    
     color:  #031014;
     display: flex;
     justify-content: center;
@@ -23,14 +21,24 @@ const FooterContainer = styled.footer`
     font-size: 20px;
   }
 
-  a:hover {
-    color:#00476D ;
+  section {
+    width: 134px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  svg {
+    width: 44px;
+    height: 44px;
+    margin-right: 8px;
   }
 
   hr {
     height: 50px;
     margin: 0px 25px 0px 25px;
-    width: 3px;
+    width: 2px;
   }
 `;
 
--- src/componentes/Header/style.js
+++ src/componentes/Header/style.js
@@ -11,7 +11,7 @@ const HeaderContainer = styled.header`
     top: 0;
     left: 0;
     right: 0;
-    box-shadow: 0px 0px 2px 0px white;    
+    box-shadow: 0px 0px 2px -1px white;    
   }
 
   p{

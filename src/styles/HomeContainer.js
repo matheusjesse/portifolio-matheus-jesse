4eb8dc87d610513137c14c9f8f2db4292ee8ce6a
matheusjesse <matheus_jesse@hotmail.com>
2023-03-21 04:59:14 -0300

    fix: arrumando o cover do gradiente na tela

--- src/styles/HomeContainer.js
+++ src/styles/HomeContainer.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
   align-content: flex-start;
-  
   background-image: linear-gradient(to right bottom, #f3f4ef, #f0f1ec, #ecede9, #e9eae6, #e6e7e3);
-
+   
+  
   h1 {
     color: #131313;
     width: 100%;
@@ -38,7 +38,8 @@ const CardMenuSection = styled.div`
   justify-content: space-around;
   flex-wrap: wrap;
   width: 80%;
-  margin-top: 174px;
+  margin-top: 144px;
+  margin-bottom: 144px;
   align-content: space-around;
 
 `;

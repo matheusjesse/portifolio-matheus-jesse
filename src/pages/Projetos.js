161d371bd10831c4ac85771a69232a7fea029b90
matheusjesse <matheus_jesse@hotmail.com>
2023-03-21 20:58:53 -0300

    chore: limpando a pagina de projetos antiga

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ProjetoContainer from '../styles/ProjetoContainer';
-import Carousel from '../componentes/DemoCarousel';
 
 function Projetos() {
-  return(
-    <>
-      <ProjetoContainer>
-        <h1 id="project-page">Projetos</h1>
-        <div className="projetoSection">
-          <Carousel />
-        </div>
-      </ProjetoContainer>
-    </>
+  return(   
+    <ProjetoContainer>
+        
+    </ProjetoContainer>
   );
 }
 
--- src/styles/ProjetoContainer.js
+++ src/styles/ProjetoContainer.js
@@ -2,37 +2,7 @@ import styled from 'styled-components';
 
 
 export const ProjetoContainer = styled.div`
-  width: 100%;
-
-  height: 76vh;
-  h1 {
-    text-align: center;
-    margin-top: 142px;
-    font-family: 'Alef', sans-serif;
-  }
-
-  .projetoSection {
-    margin: 0 20px 0px 20px;
-  }
-
-  .slick-slide > div {
-      display: grid;
-      place-items: center;
-      width: 660px;
-      margin-top: 50px;
-      margin: auto;
-      height: 500px;
-      padding: 0px;
-  } 
-
-  .slick-prev {
-    left: 20% !important;
-    z-index: 2;
-  }
-  .slick-next {
-    right: 20% !important;
-    z-index: 1;
-  }
+  
 
 `;
 

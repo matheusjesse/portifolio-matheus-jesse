c1ae4884353d952ea2b5ac57a4e1d46b89cf4a9c
matheusjesse <matheus_jesse@hotmail.com>
2023-03-23 06:01:28 -0300

    chore: adicionando efeito de hover nos menus da home

--- src/componentes/CardMenu/style.js
+++ src/componentes/CardMenu/style.js
@@ -2,43 +2,52 @@ import styled from 'styled-components';
 
 const CardMenuContainer = styled.div`
     
-    height: 128px;
-    width: 132px;
+    height: 148px;
+    width: 152px;
     margin: 20px 4px 39px 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: flex-start;
+    flex-wrap: wrap;
     :hover {
         cursor: pointer; 
     }
 
-    :hover img {
-        fill: red;
-    }
+    
 
     .hoverContent{
         display: none;
-        box-shadow: 8px 8px 22px -8px #131313;
+        margin-top: 200px;
+        visibility: hidden;
         width: 132px;
         height: 39px;
-        border: 8px solid #FEFEFE;
         border-radius: 18px;
         margin-top: 8px;
         color: #131313;
         font-family: 'Overpass', sans-serif;
         font-weight: 900;
         font-size: 12px;
+        border: 8px solid #737373;
     }
 
     :hover .hoverContent {
         display: flex;
-        box-shadow: 8px 8px 22px -8px #131313;
+        visibility: visible;
         width: 132px;
         height: 39px;
-        border: 8px solid #FEFEFE;
+        margin-top: 200px;
         border-radius: 18px;
         margin-top: 8px;
         flex-direction: row;
-        justify-content: space-between;
+        justify-content: center;
         align-items: center;
         padding: 0px 8px 0px 8px;
+        border: 8px solid transparent;
+        opacity: 1;
+        p {
+            margin-right: 4px;
+        }
     }
 
     .hoverContent p {
@@ -62,12 +71,18 @@ const ImageContainer = styled.div`
     align-items: center;
     justify-content: center;
     box-shadow: 8px 8px 22px -8px #131313;
-img svg{
-        width: 64px;
-        height: 64px;
-        fill: green !important;
+    transition: border 0.6s ;
+    transition-timing-function: ease-in-out;
+
+    :hover {
+        border: 8px solid #131313;
     }
+
 `;
+
+
+
+
 
 export default CardMenuContainer;
 export { ImageContainer };

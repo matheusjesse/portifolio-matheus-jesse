4bce26d0a770d98dc1e6a1ff6b1c93f6f6f9c213
matheusjesse <matheus_jesse@hotmail.com>
2023-08-03 20:33:43 -0300

    feat: criando o header e a secao de visão geral do projeto

--- src/pages/ResponsiveSite.js
+++ src/pages/ResponsiveSite.js
@@ -1,14 +1,53 @@
+import { D } from "styled-icons/crypto";
 import ResponsiveSiteContainer from "../styles/ResponsiveSiteContainer";
 
 function ResponsiveSite() {
   return(   
     <ResponsiveSiteContainer>
         <div className="header-page">
-            <h1>Site de Tutorial de programação</h1>
-            
+            <h1>Site Responsivo Estudo de Caso</h1>
+            <h2>Blog de tutoriais</h2>
         </div>
-        <div className="overrall">
-            
+        <div className="overall">
+            <div className="overrall-box">
+                <h3>O produto:</h3>
+                <p>
+                    CodeMT é um site de programação com tutoriais relacionados
+                     a Front-End, os principais interessados são pessoas que 
+                     estão interessados a aprender programar ou se manter 
+                     atualizado nas novidades.
+                </p>
+            </div>
+            <div className="overrall-box">
+                <h3>Duração do projeto:</h3>
+                <p>
+                    21 de junho à 07 de julho
+                </p>
+            </div>
+            <div className="overrall-box">
+                <h3>O Problema:</h3>
+                <p>
+                    Criar um site intuitivo e conciso que de fácil navegação, criando uma boa experiência para o usuário.
+                </p>
+            </div>
+            <div className="overrall-box">
+                <h3>O objetivo:</h3>
+                <p>
+                    Desenvolver um site que seja objetivo e que possa fornecer ao usuário uma boa experiência navegando entre os tutoriais e consumindo o conteúdo disponível.
+                </p>
+            </div>
+            <div className="overrall-box">
+                <h3>Minha função:</h3>
+                <p>
+                    UX leader, UX research, UX design
+                </p>
+            </div>
+            <div className="overrall-box">
+                <h3>Responsabilidades:</h3>
+                <p>
+                    Conduzir entrevista, criar wireframes de baixa e alta fidelidade, prototipos, estudos de usabilidade, considerações de acessibilidade e design responsivo. 
+                </p>
+            </div>
         </div>
     </ResponsiveSiteContainer>    
   );
--- src/styles/ResponsiveSiteContainer.js
+++ src/styles/ResponsiveSiteContainer.js
@@ -10,6 +10,36 @@ const ResponsiveSiteContainer = styled.body`
   align-content: flex-start;
   background: var(--material-theme-sys-light-surface, linear-gradient(0deg, rgba(0, 90, 193, 0.04) 0%, rgba(0, 90, 193, 0.04) 100%), #FAF9FD);
   
-   
+    .header-page {
+        height: 200px;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #112E2E;
+        flex-wrap: wrap;
+        margin-bottom: 64px;
+        align-content: center;
+    }
+
+    .header-page  h1, h2{
+        width: 100%;
+        text-align: center;
+        color: white;
+    }
+
+    .overall{
+        width: 68%;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .overrall-box{
+        width:200px;
+        margin: 14px;
+        border-radius: 12px;
+        padding: 8px;
+    }
 `
 export default ResponsiveSiteContainer;

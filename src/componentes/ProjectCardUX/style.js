8408abd35a507b6aaf6f1240bbb267857ee932f3
matheus jessé <matheus_jesse@hotmail.com>
2024-05-01 04:27:32 -0300

    style: adicionando transicao na shadow hover do card de projeto ux

--- src/componentes/ProjectCardUX/style.js
+++ src/componentes/ProjectCardUX/style.js
@@ -6,6 +6,8 @@ border: 1px solid ${props => props.theme.colors.outlineVariant};
 border-radius: 12px;
 
 cursor: pointer;
+transition: box-shadow 0.4s;
+transition-timing-function: ease-in-out;
 :hover {
         box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.2), 0px 1px 2px 0px rgba(0, 0, 0, 0.30);
     }
